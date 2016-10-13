14ba85e93f1194d91134b399a787d20a9c6fdc5b
Michele Marcucci <michele@befree.it>
2016-10-13 16:46:56 +0200

    Developing
    Added sudo to install script

--- install_minera_client.sh
+++ install_minera_client.sh
@@ -6,20 +6,20 @@ reset=`tput sgr0`
 echo -e "-----\n${green}START Minera Install script${reset}\n-----\n"
 
 echo -e "-----\n${green}Install extra packages${reset}\n-----\n"
-apt-get update
-apt-get install -y git
+sudo apt-get update
+sudo apt-get install -y git
 
 NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
 echo -e "-----\n${green}Adding Minera user${reset}\n-----\n"
-adduser minera --gecos "" --disabled-password
+sudo adduser minera --gecos "" --disabled-password
 echo "minera:$NEW_UUID" | chpasswd
 
 echo -e "-----\n${green}Adding groups to Minera${reset}\n-----\n"
-usermod -a -G dialout,plugdev,tty minera
+sudo usermod -a -G dialout,plugdev,tty minera
 
-echo -e "-----\n${green}Adding sudoers configuration for www-data and minera users\n-----\n${reset}"
-echo -e "\n#Minera settings\nminera ALL = (ALL) NOPASSWD: ALL" >> /etc/sudoers
+echo -e "-----\n${green}Adding sudoers configuration minera user\n-----\n${reset}"
+echo -e "\n#Minera settings\nminera ALL = (ALL) NOPASSWD: ALL" | sudo tee --append /etc/sudoers
 
 MINERA_DIR="/opt/minera-client"
 MINERA_ID=$1
@@ -29,22 +29,22 @@ if [ -z "$1" ]
 fi
 
 echo -e "-----\n${green}Installing NVM & Node.js${reset}\n-----\n"
-su minera -c "curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash"
-su minera -c "source /home/minera/.nvm/nvm.sh; nvm install stable"
-NODE_PATH=`su minera -c "source /home/minera/.nvm/nvm.sh; nvm which stable"`
-ln -s $NODE_PATH /usr/bin/node
+sudo su minera -c "curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash"
+sudo su minera -c "source /home/minera/.nvm/nvm.sh; nvm install stable"
+NODE_PATH=`sudo su minera -c "source /home/minera/.nvm/nvm.sh; nvm which stable"`
+sudo ln -s $NODE_PATH /usr/bin/node
 
 echo -e "-----\n${green}Cloning Minera Client${reset}\n-----\n"
-mkdir $MINERA_DIR
-git clone https://github.com/getminera/client $MINERA_DIR
-chown -R minera:minera $MINERA_DIR
+sudo mkdir $MINERA_DIR
+sudo git clone https://github.com/getminera/client $MINERA_DIR
+sudo chown -R minera:minera $MINERA_DIR
 
 echo -e "-----\n${green}Installing Node.js modules${reset}\n-----\n"
 cd $MINERA_DIR
-su minera -c "source /home/minera/.nvm/nvm.sh; npm install --production"
+sudo su minera -c "source /home/minera/.nvm/nvm.sh; npm install --production"
 
 echo -e "-----\n${green}Adding Systemd startup script${reset}\n-----\n"
-cat > /etc/systemd/system/minera-client.service <<EOL
+cat > /tmp/minera-client.service <<EOL
 [Service]
 ExecStart=/usr/bin/node $MINERA_DIR/client.js
 Restart=always
@@ -58,9 +58,11 @@ Environment=MINERA_ID=$MINERA_ID
 [Install]
 WantedBy=multi-user.target
 EOL
+sudo mv /tmp/minera-client.service /etc/systemd/system/minera-client.service
 
 echo -e "-----\n${green}Starting Minera Client${reset}\n-----\n"
-systemctl start minera-client
+sudo systemctl daemon-reload
+sudo systemctl restart minera-client
 
 if [ "$MINERA_ID" == "zTkGRKl5DHq18NdT9jNyk/ujZAH7clk+K8r7ZAOj6Kk=" ]; then
 	echo -e "${red}ATTENTION${reset} You did not add your ${red}MINERA_ID${reset}\nYou absolutely need to change it in\n    ${green}/etc/systemd/system/minera-client.service${reset}\nand restart the service with\n    ${green}sudo systemctl restart minera-client${reset}"
